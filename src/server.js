a44bbe04ac30ac66c3688e520bd33dfe76f8b0f0
carrarapets <105743712+carrarapets@users.noreply.github.com>
2022-07-02 20:11:12 -0300

    confirmar em Main

--- src/server.js
+++ src/server.js
@@ -1,7 +1,18 @@
 const express = require("express");
 const todosRoutes = require("./todos.routes");
-
+const pg = require('pg');
+require('dotenv').config();
+const connectionString =" postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}";
+const pool = new pg.Pool({
+  connectionString: isProduction ? process.env.DATABASE_URL : connectionString,
+  ssl: isProduction,
+});
+pool.on('connect', () => {
+  console.log('Teamwork Database connected successfully!');
+});
 const app = express();
+
+
 
 app.use(express.json());
 app.use(todosRoutes);
