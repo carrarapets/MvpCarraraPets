03fcb8f6dde38073180ab30992fef5f0eb68af9f
carrarapets <105743712+carrarapets@users.noreply.github.com>
2022-07-02 21:50:38 -0300

    confirmar em main

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const todosRoutes = require("./todos.routes");
 const pg = require('pg');
 require('dotenv').config();
+const isProduction = process.env.NODE_ENV ==='production';
 const connectionString ="postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}";
 const pool = new pg.Pool({
   connectionString: isProduction ? process.env.DATABASE_URL : connectionString,
