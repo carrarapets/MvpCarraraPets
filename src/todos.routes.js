78d378ac220238877a8182c7356a187e28edba22
carrapets <carrapets@gmail.com>
2022-09-19 21:47:08 -0300

    commit - rotas crud user

--- src/todos.routes.js
+++ src/todos.routes.js
@@ -9,39 +9,108 @@ const prisma = new PrismaClient();
 
 
 
-todosRoutes.post("/createuser", async (request, response)=>{
-    const{name}= request.body;
-  const todo = await  prisma.user.create({
+todosRoutes.post("/createuser", async(request, response) =>{
+    const{nome, sobrenome, cpf, celular, email, password, rg, foto}=request.body;
+    const criaUsuario = await prisma.user.create({
         data:{
-            name,
-            status: false
+            nome,
+            sobrenome,
+            cpf,
+            celular,
+            email,
+            password,
+            valido: true,
+            rg, 
+            foto
+            
+
+        }
+    });
+    return response.status(200).json(criaUsuario);
+});
+todosRoutes.get("/loginuser", async(request, response)=>{
+    const {email, password}= request.body;
+    
+        const loginUser =  await prisma.user.findFirst({
+            where:{
+                email: email,
+                password: password
+
+            }
+        })
+        
+    if(!loginUser){
+        throw new Error("Usuário/Senha incorreto")
+
+    }
+   /* const comparaSenha =  compare(password, user.password);
+    if (!comparaSenha) {
+        throw new Error("Usuário/Senha incorreto")
+        
+    }*/
+    return response.status(200).json("login efetuado com sucesso")
+})
+
+
+todosRoutes.get("/getuser/:id", async(request, response)=>{
+    const {id} = request.params;
+    const lerUsuario = await prisma.user.findUnique({
+        where:{
+            id: Number(id)
         },
+
+    })
+    return response.status(200).json(lerUsuario);
+});
+todosRoutes.get("/", (req, res) =>{
+    res.json({
+       message: "hello word"
+    });
+    
+});
+
+todosRoutes.post("/updateuser/:id", async(request, response)=>{
+    const{id} = request.params;
+    const atualizaUsuario = await prisma.user.update({
+        where:{
+            id: Number(id)
+        },
+        data:{
+            nome,
+            sobrenome,
+            cpf,
+            celular,
+            email,
+            password,
+            valido: true,
+            rg, 
+            foto
+            
+        }
+    });
+    return response.status(200).json(atualizaUsuario);
+});
+
+todosRoutes.delete("/deleteuser/:id", async(request, response)=>{
+    const{id}= request.params;
+    const deletaUsuario = await prisma.user.delete({
+        where:{
+            id: Number(id)
+        },
+        data:{
+            nome,
+            sobrenome,
+            cpf,
+            celular,
+            email,
+            password,
+            valido: true,
+            rg, 
+            foto
+            
+        }
 
     });
-    //allTodos.push({name, status: false})
-    return response.status(201).json(todo);
-
-});
-
-
-todosRoutes.get("/getuser",  async (request, response)=>{
-    const lerUser = await prisma.user.findMany({})
-    
-    return response.status(200).json(lerUser);
-});
-todosRoutes.get("/user/:id",  async (request, response)=>{
-const  {id} = request.params;
-    const lerUser = await prisma.user.findUnique({
-        where:{
-            if (condition) {
-                id: Number(id) 
-            }
-            
-            
-        },
-    })
-    
-    return response.status(200).json(lerUser);
 });
 
 
