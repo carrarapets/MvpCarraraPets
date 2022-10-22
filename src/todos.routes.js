0099645f7d58e38071fc396fdb67481449cdc02a
myusername <myemail>
2022-10-22 15:26:32 -0300

    FIX/Importando a classe RuleValidation

--- src/todos.routes.js
+++ src/todos.routes.js
@@ -18,7 +18,7 @@ todosRoutes.post("/createuser", async (request, response) => {
     const { nome, sobrenome, cpf, celular, email, password, rg, foto } = request.body;
         
     const emailValidate = RuleValidation.validationEmail(email);
-    const emailAlready = RuleValidation.emailAlreadyExist(email);
+//    const emailAlready = RuleValidation.emailAlreadyExist(email);
     const celularValidate = RuleValidation.validationPhone(celular);
 //  const celularAlready = RuleValidation.phoneAlreadyExist(celular);
     const documentCpfValidate = RuleValidation.validationCpfDocument(cpf);      
@@ -28,7 +28,7 @@ todosRoutes.post("/createuser", async (request, response) => {
         
         if (emailValidate === false) {
             throw new Error("Email Inválido!")
-        } else if (emailAlready != null) {
+//        } else if (emailAlready === false) {
 //            throw new Error("Email já cadastrado!", console.log(emailAlready))
             return response.status(201).json(emailAlready);
         } else if (celularValidate === false) {
@@ -122,7 +122,7 @@ todosRoutes.post("/updateuser/:id", async(request, response)=>{
         const { id } = request.params;
         
         const emailValidate = RuleValidation.validationEmail(email);
-        const emailAlready = RuleValidation.emailAlreadyExist(email);
+  //      const emailAlready = RuleValidation.emailAlreadyExist(email);
         const celularValidate = RuleValidation.validationPhone(celular);
         //  const celularAlready = RuleValidation.phoneAlreadyExist(celular);
         const documentCpfValidate = RuleValidation.validationCpfDocument(cpf);
@@ -132,9 +132,9 @@ todosRoutes.post("/updateuser/:id", async(request, response)=>{
         
         if (emailValidate === false) {
             throw new Error("Email Inválido!")
-                   }
-                   else if (emailAlready == false) {
-                      throw new Error("Email já cadastrado!")
+        //           }
+        //           else if (emailAlready == false) {
+        //             throw new Error("Email já cadastrado!")
         } else if (celularValidate === false) {
             throw new Error("Celular Inválido!")
             //            } else if (celularAlready == false) {
